f3781e97d25b3762d36b259bd4273369d0f068c2
Ahmed Yousef <AYousef@steelnetwork.com>
2025-10-14 07:23:52 +0300

    Refactor and add EF Core infrastructure layer
    Updated `DeleteOrderCommandHandler` and `IOrderRepository` to use synchronous methods for entity state changes, aligning with EF Core's design. Modified `OrderItem` to enforce immutability with private setters.
    
    Introduced `InfrastructureServiceRegistration` for dependency injection, registering `DbContext`, `IOrderRepository`, and `IUnitOfWork`. Added EF Core migrations to define `Orders` and `OrderItems` tables and relationships. Implemented `ApplicationDbContext`, `OrderRepository`, and `UnitOfWork` for database operations.
    
    Updated `Infrastructure.csproj` and `WebApi.csproj` to include EF Core dependencies and project references. Configured SQL Server connection string in `appsettings.json`. Integrated infrastructure layer into the `WebApi` project.

--- HexagonalOrderManagement/Core/Application/Ports/IOrderRepository.cs
+++ HexagonalOrderManagement/Core/Application/Ports/IOrderRepository.cs
@@ -11,8 +11,16 @@ public interface IOrderRepository
 
     Task AddAsync(Order? order, CancellationToken cancellationToken = default);
 
-    Task UpdateAync(Order? order, CancellationToken cancellationToken = default);
+    void Update(Order? order, CancellationToken cancellationToken = default);
 
-    Task DeleteAsync(Order? order, CancellationToken cancellationToken = default);
+    void Delete(Order? order, CancellationToken cancellationToken = default);
 
 }
+//Why EF Core’s Update() and Remove() are synchronous
+// . EF Core's Update() and Remove() methods are synchronous because they
+// primarily involve modifying the state of the in-memory entity
+// tracking system rather than performing any I/O-bound operations.
+// These methods mark entities as modified or deleted in the context,
+// which is a quick operation that doesn't require waiting for external resources
+// like a database. The actual database operations occur when SaveChangesAsync() is called,
+// which is asynchronous to efficiently handle I/O operations without blocking the calling thread.
---
+++ HexagonalOrderManagement/Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Core.Application.Ports;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class UnitOfWork : IUintOfWork
+{
+    private readonly ApplicationDbContext _context;
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+}
